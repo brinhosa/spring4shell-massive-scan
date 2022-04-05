c620811aa2fab3e3e05ff662094587ac39e08315
Rafael <rafael@brinhosa.com.br>
2022-04-05 13:53:35 -0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,49 +2,49 @@
 
 for url  in $(cat $1)
 do
-        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.URLs%5B0%5D=0" )
+        status_code=$(curl -L -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.URLs%5B0%5D=0" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ + ] Vulnerable enviroment: $url"
         fi
-        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.DefaultAssertionStatus=test" )
+        status_code=$(curl -L -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.DefaultAssertionStatus=test" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ ++ ] Vulnerable enviroment: $url"
         fi        
-        status_code=$(curl -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url" )
+        status_code=$(curl -L -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ +P ] Vulnerable enviroment: $url"
         fi        
-        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url/actuator/info?class.module.classLoader.URLs%5B0%5D=0" )
+        status_code=$(curl -L -s -o /dev/null -w "%{http_code}" "$url/actuator/info?class.module.classLoader.URLs%5B0%5D=0" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ +I ] Vulnerable enviroment: $url"
         fi
-        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url/system/mappings?class.module.classLoader.URLs%5B0%5D=0" )
+        status_code=$(curl -L -s -o /dev/null -w "%{http_code}" "$url/system/mappings?class.module.classLoader.URLs%5B0%5D=0" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ +M ] Vulnerable enviroment: $url"
         fi       
-        status_code=$(curl -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url/system/showOsInfo" )
+        status_code=$(curl -L -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url/system/showOsInfo" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ +S ] Vulnerable enviroment: $url"
         fi        
-        status_code=$(curl -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url/actuator/health" )
+        status_code=$(curl -L -s --data "class.module.classLoader.URLs%5B0%5D=0" -o /dev/null -w "%{http_code}" "$url/actuator/health" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ +H ] Vulnerable enviroment: $url"
         fi         
-        status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url/peripheral/v2/api-docs?class.module.classLoader.URLs%5B0%5D=0" )
+        status_code=$(curl -L -s -o /dev/null -w "%{http_code}" "$url/peripheral/v2/api-docs?class.module.classLoader.URLs%5B0%5D=0" )
         echo $status_code , $url 
         if [ $status_code == "400" ]
         then
