43029d61044cc96d22b66b2f0fa495cc61e6a9a4
Rafael <rafael@brinhosa.com.br>
2022-04-05 11:29:18 -0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -3,13 +3,13 @@
 for url  in $(cat $1)
 do
         status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.URLs%5B0%5D=0" )
-        echo $url $status_code
+        echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ + ] Vulnerable enviroment: $url"
         fi
         status_code=$(curl -s -o /dev/null -w "%{http_code}" "$url?class.module.classLoader.DefaultAssertionStatus=test" )
-        echo $url $status_code
+        echo $status_code , $url 
         if [ $status_code == "400" ]
         then
                 echo "[ ++ ] Vulnerable enviroment: $url"
